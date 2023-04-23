5fdd0b268c79cd48588511e8dc6d580b512197ee
David Godswill Ilerioluwa <76020967+davidilerioluwa@users.noreply.github.com>
2023-04-23 07:33:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import cors from "cors"
-import dalleRoutes from "./routes/dalle.routes.js"
-import path from "path"
-import {fileURLToPath} from 'url';
+// import express from "express"
+const express= require("express")
+// import * as dotenv from "dotenv"
+const dotenv= require("dotenv")
+// import cors from "cors"
+const cors = require("cors")
+// import dalleRoutes from "./routes/dalle.routes.js"
+// const dalleRoutes= require("./routes/dalle.routes.js")
+// import path from "path"
+const path =require("path")
+// import {fileURLToPath} from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 
 // 👇️ "/home/john/Desktop/javascript"
-const __dirname = path.dirname(__filename);
-console.log('directory-name 👉️', __dirname);
+// const __dirname = path.dirname(__filename);
+// console.log('directory-name 👉️', __dirname);
 
 // 👇️ "/home/borislav/Desktop/javascript/dist/index.html"
 // console.log(path.join(__dirname, '/dist', 'index.html'))
@@ -24,7 +29,7 @@ app.get("/*",(req,res)=>{
 app.use(cors())
 app.use(express.json({limit:"50mb"}))
 
-app.use('/api/v1/dalle', dalleRoutes);
+// app.use('/api/v1/dalle', dalleRoutes);
 
 app.get("/",(req,res)=>{
     res.status(200).json({message:"hello"})
